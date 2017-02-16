1eedceb095223d7bd0cc621454f5b809ebe84984
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2017-02-16 09:57:08 +0100

    fix code

--- little-switches/arduino/2_setup-pot/2_setup-pot.ino
+++ little-switches/arduino/2_setup-pot/2_setup-pot.ino
@@ -3,12 +3,8 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("A0: ");
-  Serial.print(analogRead(A0));
-  Serial.print(" - A1: ");
-  Serial.print(analogRead(A1));
-  Serial.print(" - A2: ");
-  Serial.print(analogRead(A2));
-  Serial.print(" - A3: ");
-  Serial.print(analogRead(A3));
+  
+  Serial.print("A3: ");
+  Serial.println(analogRead(A3));
+  delay(500);
 }
