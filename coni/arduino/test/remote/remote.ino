7159ba5377c93ba2cd9b35d75993c03238b8069f
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2017-03-05 21:20:10 +0100

    first test

--- coni/arduino/test/remote/remote.ino
+++ coni/arduino/test/remote/remote.ino
@@ -1,30 +1,28 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(53,OUTPUT);
-  pinMode(52,OUTPUT);
-  pinMode(51,OUTPUT);
-  pinMode(50,OUTPUT);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
 
   Serial.begin(9600);
   Serial2.begin(4800);
 
-  pinMode(12,INPUT_PULLUP);
-  
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+
 }
 
 bool oki = false;
+uint32_t dataOut = 0;
+long dataIn = 0;
+char tag;
+bool inv = false;
+
+String inString = "";
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (digitalRead(12)) {
-    digitalWrite(52,HIGH);
-    if (oki) {
-      Serial2.println("ciao");
-      oki = false;
-    }
-  }
-  else {
-    digitalWrite(52,LOW);
-    oki = true;
-  }
+  
+  Serial2.write(Serial.read());
+
 }
--- coni/arduino/test/small/small.ino
+++ coni/arduino/test/small/small.ino
@@ -1,63 +1,147 @@
-#include <AccelStepper.h>
 #include <SoftwareSerial.h>
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+#include <SerialCommand.h>
 
-#define RXpin 11
-#define TXpin 12
 
-#define pinStep_A 9
-#define pinDirection_A 10
+SerialCommand SCmd;
 
-#define pinStep_B 3
-#define pinDirection_B 4
 
-#define pinSens_A A0
-#define pinSens_B A5
+#define pinRX 11
+#define pinTX 12
+
+//MOTORE STRETTO
+#define pinStep_A 4        //bianco
+#define pinDirection_A 5   //giallo
+#define pinSens_A A2
+
+//MOTORE LARGO
+#define pinStep_B 9         //bianco
+#define pinDirection_B 10   //giallop
+#define pinSens_B A0
 
 #define RE 6
 #define DE 7
 
-SoftwareSerial mySerial(pinRX, pinTX);
-AccelStepper stepper_A(1, pinStep_A, pinDirection_A); 
-AccelStepper stepper_B(1, pinStep_B, pinDirection_B); 
+#define MAXSPEED 5000
+#define MAXACC 800
+
+AccelStepper stepper_A(1, pinStep_A, pinDirection_A);
+AccelStepper stepper_B(1, pinStep_B, pinDirection_B);
+
+MultiStepper steppers;
+
+long posA = 0;
+long posB = 0;
+
+long positions[2] = {0,0};
+
+uint32_t velA = 1000;
+uint32_t velB = 1000;
 
 uint32_t dataIn = 0;
 
-void setup()
-{  
-  mySerial.begin(4800);
+void setup() {
+
+  SCmd.addCommand("p",process_command);
+  SCmd.addCommand("P",Process_command);
+
+  stepper_A.setMaxSpeed(MAXSPEED);
+  stepper_B.setMaxSpeed(MAXSPEED);
+  stepper_A.setAcceleration(MAXACC);
+  stepper_B.setAcceleration(MAXACC);
+
+  steppers.addStepper(stepper_A);
+  steppers.addStepper(stepper_B);
+
+  //200 step per rev  (motor)
+  //1600 pulse per rev (driver)
+  pinMode(pinSens_A, INPUT_PULLUP);
+  pinMode(pinSens_B, INPUT_PULLUP);
 
   pinMode(RE, OUTPUT);
   pinMode(DE, OUTPUT);
   digitalWrite(RE, LOW);
   digitalWrite(DE, LOW);
 
-  //200 step per rivoluzione
-  //1600 pulse per rivoluzione
-
-  stepper_A.setMaxSpeed(1000);
-  stepper_A.setAcceleration(1000);
-
-  goToZero();
+  Serial.begin(9600);
 
 }
 
-void loop()
+
+void loop() {
+  SCmd.readSerial();
+  steppers.moveTo(positions);
+  steppers.runSpeedToPosition();
+
+}
+
+void process_command()
 {
-  if (mySerial.available()) {
-    dataIn = mySerial.parseInt();
-    if (dataIn > 5000) {
-    	distance = dataIn *;
-    	velocity = dataIn *;
-    }
-  }
-  
-  stepper.moveTo(distance);
-  stepper.setSpeed(velocity);
-  stepper.runSpeedToPosition();
+  int aNumber;
+  char *arg;
 
-  
+  Serial.println("We're in process_command");
+  arg = SCmd.next();
+  if (arg != NULL)
+  {
+    aNumber=atoi(arg);    // Converts a char string to an integer
+    positions[0] += (long)(aNumber * 100800 / 360);
+    Serial.print("First argument was: ");
+    Serial.println(positions[0]);
+
+  }
+  else {
+    Serial.println("No arguments");
+  }
+
+  arg = SCmd.next();
+  if (arg != NULL)
+  {
+    aNumber=atoi(arg);
+    positions[1] += (long)(aNumber * 100800 / 360);
+    Serial.print("Second argument was: ");
+    Serial.println(positions[1]);
+
+
+  }
+  else {
+    Serial.println("No second argument");
+  }
+
 }
 
-void goToZero() {
-	while (analogRead(pinSens_A) > 0)
+void Process_command()
+{
+  int aNumber;
+  char *arg;
+
+  Serial.println("We're in process_command");
+  arg = SCmd.next();
+  if (arg != NULL)
+  {
+    aNumber=atoi(arg);    // Converts a char string to an integer
+    stepper_A.setMaxSpeed(aNumber);
+    Serial.print("Second argument was: ");
+    Serial.println(aNumber);
+
+  }
+  else {
+    Serial.println("No arguments");
+  }
+
+  arg = SCmd.next();
+  if (arg != NULL)
+  {
+    aNumber=atoi(arg);
+    stepper_B.setMaxSpeed(aNumber);
+    Serial.print("Second argument was: ");
+    Serial.println(aNumber);
+
+
+  }
+  else {
+    Serial.println("No second argument");
+  }
+
 }
